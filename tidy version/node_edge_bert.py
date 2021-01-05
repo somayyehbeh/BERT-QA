51832568590aaf79c9d516cd1da91835b65e8e33
meti-94 <mehdi_j94@outlook.com>
2021-01-05 22:08:43 +0330

    adding Dataset and saving/load functionality

--- tidy version/node_edge_bert.py
+++ tidy version/node_edge_bert.py
@@ -1,5 +1,8 @@
 from transformers import BertModel, BertTokenizer
 import torch
+from torch.utils.data import Dataset, DataLoader
+import os
+import numpy as np 
 
 class NodeEdgeDetector(torch.nn.Module):
     def __init__(self, bert, dropout=0., **kw):
@@ -27,10 +30,45 @@ class NodeEdgeDetector(torch.nn.Module):
                             logits_edge_start.transpose(1, 2), logits_edge_end.transpose(1, 2)], 1)
         return logits
 
+
+class BordersDataset(Dataset):
+	def __init__(self, path='./bertified/'):
+		# loading datasets into the memory
+		self.tokens_matrix = np.load(os.path.join(path, 'tokenmat.npy'))
+		self.nods_borders = np.load(os.path.join(path, 'entities.npy'))
+		self.edgs_borders = np.load(os.path.join(path, 'relations.npy'))
+		# convert into tensors
+		self.tokens_matrix = torch.from_numpy(self.tokens_matrix).long()
+		self.borders = torch.from_numpy(np.concatenate((self.nods_borders, self.edgs_borders),
+														 axis=1)).long()		
+		self.n_samples = self.borders.shape[0]
+
+	def __getitem__(self, index):
+		# returns specific item
+		return self.tokens_matrix[index], self.borders[index] 
+	def __len__(self):
+		return self.n_samples
+		# returns dataset length
+
+
+
 if __name__=='__main__':
-    random_input = torch.randint(3, 20000, (5, 15)).long()
-    bert = BertModel.from_pretrained("bert-base-uncased")
-    node_edge_detector = NodeEdgeDetector(bert, dropout=torch.tensor(0.5))
-    output = node_edge_detector(random_input)
-    print(output.size())
-    print(output)
+    # dataset = BordersDataset()
+    # dataloader = DataLoader(dataset=dataset, batch_size=5, shuffle=True)
+    # dataiter = iter(dataloader)
+    # data = dataiter.next()
+    # featurs, labels = data
+    # print(featurs.size, labels.size, labels)
+    # random_input = torch.randint(3, 20000, (5, 15)).long()
+    # bert = BertModel.from_pretrained("bert-base-uncased")
+    # node_edge_detector = NodeEdgeDetector(bert, dropout=torch.tensor(0.5))
+    # output = node_edge_detector(random_input)
+    # print(output.size())
+    # print(output)
+    # torch.save(node_edge_detector, './models/save_test.pt')
+    # model = torch.load('./models/save_test.pt')
+    # model.eval()
+    # output = model(random_input)
+    # print(output.size())
+    # print(output)
+    
