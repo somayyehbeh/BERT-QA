32ccbe32323288db744d190733f94a0e490b2af6
unknown <mehdi_j94@outlook.com>
2021-01-08 01:40:19 +0330

    bert with 4 border heads compeleted!

--- tidy version/node_edge_bert.py
+++ tidy version/node_edge_bert.py
@@ -1,47 +1,54 @@
 from transformers import BertModel, BertTokenizer
+from transformers import AdamW
 import torch
+from torch.nn.functional import nll_loss
 from torch.utils.data import Dataset, DataLoader
-import os
+from utils import read_data, get_f1
+from torch.nn.functional import mse_loss
+from torch.nn.functional import one_hot
 import numpy as np 
 
-class NodeEdgeDetector(torch.nn.Module):
-    def __init__(self, bert, dropout=0., **kw):
-        super().__init__(**kw)
-        self.bert = bert
-        dim = self.bert.config.hidden_size
-        self.nodestart = torch.nn.Linear(dim, 1)
-        self.nodeend = torch.nn.Linear(dim, 1)
-        
-        self.edgestart = torch.nn.Linear(dim, 1)
-        self.edgeend = torch.nn.Linear(dim, 1)
-        self.dropout = torch.nn.Dropout(p=dropout)
 
-    def forward(self, x):       # x: (batsize, seqlen) ints
-        mask = (x != 0).long()
-        bert_outputs = self.bert(x, attention_mask=mask, output_hidden_states=False)
-        lhs = bert_outputs.last_hidden_state
-        a = self.dropout(lhs)
-        logits_node_start = self.nodestart(lhs)
-        logits_node_end = self.nodeend(lhs)
-        logits_edge_start = self.edgestart(lhs)
-        logits_edge_end = self.edgeend(lhs)
-        
-        logits = torch.cat([logits_node_start.transpose(1, 2), logits_node_end.transpose(1, 2), 
-                            logits_edge_start.transpose(1, 2), logits_edge_end.transpose(1, 2)], 1)
-        return logits
+
+class NodeEdgeDetector(torch.nn.Module):
+	def __init__(self, bert, dropout=0.5, clip_len=True, **kw):
+		super().__init__(**kw)
+		self.bert = bert
+		dim = self.bert.config.hidden_size
+		self.nodestart = torch.nn.Linear(dim, 1)
+		self.nodeend = torch.nn.Linear(dim, 1)
+		
+		self.edgestart = torch.nn.Linear(dim, 1)
+		self.edgeend = torch.nn.Linear(dim, 1)
+		self.dropout = torch.nn.Dropout(p=dropout)
+		self.clip_len = clip_len
+
+	def forward(self, x):	   # x: (batsize, seqlen) ints
+		mask = (x != 0).long()
+		if self.clip_len:
+			maxlen = mask.sum(1).max().item()
+			maxlen = min(x.size(1), maxlen + 1)
+			mask = mask[:, :maxlen]
+			x = x[:, :maxlen]
+		bert_outputs = self.bert(x, attention_mask=mask, output_hidden_states=False)
+		lhs = bert_outputs.last_hidden_state
+		a = self.dropout(lhs)
+		logits_node_start = self.nodestart(lhs)
+		logits_node_end = self.nodeend(lhs)
+		logits_edge_start = self.edgestart(lhs)
+		logits_edge_end = self.edgeend(lhs)
+		
+		logits = torch.cat([logits_node_start.transpose(1, 2), logits_node_end.transpose(1, 2), 
+							logits_edge_start.transpose(1, 2), logits_edge_end.transpose(1, 2)], 1)
+		return logits
 
 
 class BordersDataset(Dataset):
-	def __init__(self, path='./bertified/'):
-		# loading datasets into the memory
-		self.tokens_matrix = np.load(os.path.join(path, 'tokenmat.npy'))
-		self.nods_borders = np.load(os.path.join(path, 'entities.npy'))
-		self.edgs_borders = np.load(os.path.join(path, 'relations.npy'))
+	def __init__(self, data):
 		# convert into tensors
-		self.tokens_matrix = torch.from_numpy(self.tokens_matrix).long()
-		self.borders = torch.from_numpy(np.concatenate((self.nods_borders, self.edgs_borders),
-														 axis=1)).long()		
-		self.n_samples = self.borders.shape[0]
+		self.tokens_matrix = torch.from_numpy(data[0]).long()
+		self.borders = torch.from_numpy(data[1]).long()
+		self.n_samples = data[0].shape[0]
 
 	def __getitem__(self, index):
 		# returns specific item
@@ -52,23 +59,132 @@ class BordersDataset(Dataset):
 
 
 
-if __name__=='__main__':
-    # dataset = BordersDataset()
-    # dataloader = DataLoader(dataset=dataset, batch_size=5, shuffle=True)
-    # dataiter = iter(dataloader)
-    # data = dataiter.next()
-    # featurs, labels = data
-    # print(featurs.size, labels.size, labels)
-    # random_input = torch.randint(3, 20000, (5, 15)).long()
-    # bert = BertModel.from_pretrained("bert-base-uncased")
-    # node_edge_detector = NodeEdgeDetector(bert, dropout=torch.tensor(0.5))
-    # output = node_edge_detector(random_input)
-    # print(output.size())
-    # print(output)
-    # torch.save(node_edge_detector, './models/save_test.pt')
-    # model = torch.load('./models/save_test.pt')
-    # model.eval()
-    # output = model(random_input)
-    # print(output.size())
-    # print(output)
+class TrainingLoop:
+	def __init__(self, model, optimizer, freezeemb=True, 
+				 epochs=6, save_path='./models/', **kw):
+		self.model = model
+		params = []
+		for paramname, param in self.model.named_parameters():
+			if paramname.startswith("bert.embeddings.word_embeddings"):
+				if not freezeemb:
+					params.append(param)
+			else:
+				params.append(param)
+		self.optimizer = optimizer(params, **kw)
+		self.epochs = epochs
+		self.save_path = save_path
+		self.predicts = None
+
+	def train(self, dataloader, eval_dataloader, loss_function):
+		device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+		self.model.to(device)
+		
+		for epoch in range(self.epochs):
+			self.model.train()
+			losses = []
+			for batch in dataloader:
+				self.model.zero_grad()
+				X, y = batch
+				X = X.to(device); y = y.to(device)
+				logits = self.model(X) # [4, 4, 31]
+				loss = loss_function(logits, one_hot(y, num_classes=logits.size()[-1]).float(),
+                             reduction='sum')
+				losses.append(loss)
+				loss.backward()
+				self.optimizer.step()
+			print(f'Epoch number: {epoch+1} Train Loss is equal: {sum(losses)/len(losses)}') 
+			self.eval(eval_dataloader, loss_function, epoch, device)
+
+
+	def eval(self, dataloader, loss_function, epoch, device):
+		self.model.eval()
+		losses = []
+		for batch in dataloader:
+			with torch.no_grad():
+				X, y = batch
+				X = X.to(device); y = y.to(device)
+				logits = self.model(X)
+				loss = loss_function(logits, one_hot(y, num_classes=logits.size()[-1]).float(),
+                             reduction='sum')
+				losses.append(loss)
     
+		# cliped_logits = X[0][:]
+		# print(logits.size(), torch.argmax(logits[0:10], dim=2))
+		print(f'Epoch number: {epoch+1} Eval Loss is equal: {sum(losses)/len(losses)}')
+
+	def predict(self, dataloader, device, evaluate=True):
+		self.model.eval()
+		predicts = []
+		for batch in dataloader:
+			with torch.no_grad():
+				X, _ = batch
+				X = X.to(device)
+				logits = self.model(X)
+				borders = torch.argmax(logits, dim=2).cpu().detach().numpy().tolist()
+				[predicts.append(item) for item in borders]
+		predicts = np.array(predicts)
+		self.predicts = predicts
+		if evaluate:
+			goldens = []
+			for batch in dataloader:
+				_, y = batch
+				borders = y.cpu().detach().numpy().tolist()
+				[goldens.append(item) for item in borders]
+			goldens = np.array(goldens)
+			gold_nodes_border = goldens[:, :2]
+			gold_edges_border = goldens[:, 2:]
+			pred_nodes_border = self.predicts[:, :2]
+			pred_edges_border = self.predicts[:, 2:]
+			get_f1(pred_nodes_border, gold_nodes_border)
+			get_f1(pred_edges_border, gold_edges_border)
+
+
+
+
+      
+
+
+
+
+
+
+
+if __name__=='__main__':
+	train, valid, test = read_data()
+	bert = BertModel.from_pretrained("bert-base-uncased")
+	node_edge_detector = NodeEdgeDetector(bert, dropout=torch.tensor(0.5))
+	optimizer = AdamW
+	kw = {'lr':0.0002, 'weight_decay':0.1}
+	tl = TrainingLoop(node_edge_detector, optimizer, True, **kw)
+	
+	train_dataset = BordersDataset(train)
+	train_dataloader = DataLoader(dataset=train_dataset, batch_size=400, shuffle=True, pin_memory=True)
+	valid_dataset = BordersDataset(valid)
+	valid_dataloader = DataLoader(dataset=valid_dataset, batch_size=400, shuffle=False, pin_memory=True)
+	test_dataset = BordersDataset(test)
+	test_dataloader = DataLoader(dataset=test_dataset, batch_size=400, shuffle=False, pin_memory=True)
+	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+	loss = mse_loss
+	tl.train(train_dataloader, valid_dataloader, loss)
+	tl.predict(test_dataloader, device)
+	
+	##########################################################
+	# dataset = BordersDataset()
+	# dataloader = DataLoader(dataset=dataset, batch_size=5, shuffle=True)
+	# dataiter = iter(dataloader)
+	# data = dataiter.next()
+	# featurs, labels = data
+	# print(featurs.size, labels.size, labels)
+	# random_input = torch.randint(3, 20000, (5, 15)).long()
+	# bert = BertModel.from_pretrained("bert-base-uncased")
+	# node_edge_detector = NodeEdgeDetector(bert, dropout=torch.tensor(0.5))
+	# output = node_edge_detector(random_input)
+	# print(output.size())
+	# print(output)
+	# torch.save(node_edge_detector, './models/save_test.pt')
+	# model = torch.load('./models/save_test.pt')
+	# model.eval()
+	# output = model(random_input)
+	# print(output.size())
+	# print(output)
+	###################################################
---
+++ tidy version/utils.py
@@ -0,0 +1,44 @@
+from sklearn.model_selection import train_test_split
+import numpy as np
+import os
+
+def read_data(path='./bertified/'):
+	tokens_matrix = np.load(os.path.join(path, 'tokenmat.npy'))
+	nods_borders = np.load(os.path.join(path, 'entities.npy'))
+	edgs_borders = np.load(os.path.join(path, 'relations.npy'))
+	borders = np.concatenate((nods_borders, edgs_borders), axis=1)
+	X_train, X_test, y_train, y_test = train_test_split(tokens_matrix, borders, test_size=0.30, random_state=42)
+	X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=0.15, random_state=42)
+	return (X_train, y_train), (X_valid, y_valid), (X_test, y_test)		
+
+def get_f1(predicts, golden):
+	gold_start, gold_end = golden[:, 0], golden[:, 1]
+	pred_start, pred_end = predicts[:, 0], predicts[:, 1]
+	overlap_start = np.maximum(pred_start, gold_start)
+	overlap_end = np.minimum(pred_end, gold_end)
+	overlap = (overlap_end - overlap_start).sum()
+	expected = (gold_end - gold_start).sum()
+	predicted = (pred_end - pred_start).sum()
+	recall = overlap / expected
+	precision = overlap / predicted
+	f1 = 2 * recall * precision / (recall + precision)
+	print("Dataset-wide F1, precision and recall:")
+	print(f1.item(), precision.item(), recall.item())
+	overlap = (overlap_end - overlap_start)
+	recall = overlap / (gold_end - gold_start)
+	precision = overlap / (pred_end - pred_start)
+	f1 = 2 * recall * precision / (recall + precision)
+	# print((recall > 1).nonzero(), (precision > 1).nonzero())
+	recall = recall.mean()
+	precision = precision.mean()
+	acc = (f1 == 1).mean()
+	f1 = f1.mean()
+	print("Averaged F1, precision and recall:")
+	print(f1.item(), precision.item(), recall.item())
+	print("Span accuracy")
+	print(acc)
+
+if __name__=='__main__':
+	train, valid, test = read_data()
+	get_f1(valid[1][:, :2], valid[1][:, :2])
+	print(train[0].shape)
