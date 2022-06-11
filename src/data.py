1961ae827fd4ee2c67ef21ddf26d9f623838cb49
MehdiJafari <mehdi_j94@outlook.com>
2022-06-11 22:22:57 +0430

    just after adding SQ bertified and read

--- src/data.py
+++ src/data.py
@@ -12,7 +12,9 @@ from transformers import BertTokenizer
 from itertools import chain, combinations
 from sklearn.model_selection import train_test_split
 from tqdm import tqdm
-
+import sys
+import torch
+from torch.utils.data import TensorDataset
 
 QUESTION_WORDS = ['what', 'which', 'where', 'when', 'why', 'who', 'how', 'whom']
 
@@ -122,9 +124,6 @@ def get_question_words_ids():
 	question_words_ids += [101, 102]
 	return question_words_ids
 	
-
-
-
 def create_bertified_dataset( input_excel_dir = r'../data/',
 							  output_pkl_dir = r'../bertified/',
 							  data_folder = r'data'):
@@ -178,6 +177,67 @@ def create_bertified_dataset( input_excel_dir = r'../data/',
 		np.save(f, entity_borders)
 	with open(os.path.join(output_pkl_dir,'relations.npy'), 'wb') as f:
 		np.save(f, relation_borders)
+
+
+def SQ_create_bertified_dataset( input_excel_dir = r'../data/',
+							  output_pkl_dir = r'../bertified/SQ/',
+							  data_folder = r'data'):
+	'''
+	This is where all the magics have happened. 
+	the function is designed to convert inter-mediate data to what BERT network can be fed by.
+	'''
+	dataframe = pd.read_excel(os.path.join(input_excel_dir, 'test_useful_records.xlsx'), engine='openpyxl')
+	# retrieving maximum length sample
+	maxlen = dataframe['token_matrix'].apply(lambda x:len(eval(x))).max()
+	# converting the data into nd.numpyarray 
+	token_mat = np.zeros((len(dataframe), maxlen), dtype="int32")
+
+	for i, row in tqdm(enumerate(dataframe['token_matrix'].to_list()), total=len(dataframe), desc='token matrix indexing ...'):
+		token_mat[i, :len(eval(row))] = eval(row)
+
+	# adding labels for start|end of entites. 
+	entity_borders = np.zeros((len(dataframe), 2), dtype='int16')
+	for i, (bigger, ent1, ent2) in tqdm(enumerate(zip(dataframe['token_matrix'].to_list(), 
+										   dataframe['first_entity_ids'].to_list(),
+										   dataframe['second_entity_ids'].to_list())),
+										total=len(dataframe), 
+										desc='entity indexing ...'):
+
+		temp1 = eval(ent1)[1:-1]
+		entity_borders[i] = [-1, -1]
+		for j in range(len(eval(bigger))-len(temp1)):
+			if (eval(bigger)[j:j+len(temp1)] == temp1):
+				entity_borders[i] = [j+1, j+len(temp1)+1]
+	# adding labels for relation spans 
+	relation_borders = np.zeros((len(dataframe), maxlen), dtype='int32')
+	question_words_ids = get_question_words_ids()
+	for i, (token_array, ent_borders) in tqdm(enumerate(zip(dataframe['token_matrix'].to_list(), 
+											  		   entity_borders)),
+											   total=len(dataframe), 
+												desc='determining entity boarders ...'):
+		relation_borders[i, :len(eval(token_array))] = get_relation(eval(token_array), ent_borders, question_words_ids)
+	dumb_samples = []
+	# filtering out dumb samples (samples with no / defected labels)
+	for i, (tokens, relation, entity) in enumerate(zip(token_mat, relation_borders, entity_borders)):
+		if sum(relation)==0 or entity[0]==entity[-1]:
+			dumb_samples.append(i)
+	dumb_records = dataframe.iloc[dumb_samples, :]
+	dumb_records.to_excel(os.path.join(input_excel_dir, 'dumb_records.xlsx'))
+	useful_records = dataframe[~(dataframe.index.isin(dumb_samples))]
+	# splitting usefull records into train|valid|test
+	train, test = train_test_split(useful_records, test_size=0.30, random_state=42)
+	train, valid = train_test_split(train, test_size=0.15, random_state=42)
+	train.to_excel(os.path.join(input_excel_dir, 'train.xlsx')); valid.to_excel(os.path.join(input_excel_dir, 'valid.xlsx')); test.to_excel(os.path.join(input_excel_dir, 'test.xlsx'))
+	relation_borders = np.delete(relation_borders, dumb_samples, axis=0)
+	entity_borders = np.delete(entity_borders, dumb_samples, axis=0)
+	token_mat = np.delete(token_mat, dumb_samples, axis=0)
+	# saving the results 
+	with open(os.path.join(output_pkl_dir, 'test_tokenmat.npy'), 'wb') as f:
+		np.save(f, token_mat)
+	with open(os.path.join(output_pkl_dir, 'test_entities.npy'), 'wb') as f:
+		np.save(f, entity_borders)
+	with open(os.path.join(output_pkl_dir,'test_relations.npy'), 'wb') as f:
+		np.save(f, relation_borders)
 			
 def read_data(token_path=r'../bertified/tokenmat.npy',	
 			relation_path=r'../bertified/relations.npy', 
@@ -197,6 +257,19 @@ def read_data(token_path=r'../bertified/tokenmat.npy',
 
 	ret = (traindata, devdata, testdata)
 	return ret
+
+def read_data(datatype):
+	token_path=f'../bertified/SQ/{datatype}_tokenmat.npy'
+	relation_path=f'../bertified/SQ/{datatype}_relations.npy' 
+	entity_path=f'../bertified/SQ/{datatype}_entities.npy'
+	
+	tokens = np.load(token_path)
+	relations = np.load(relation_path)
+	entities = np.load(entity_path)
+	labels = np.hstack((entities, relations))
+	selected = [torch.from_numpy(tokens).long(), torch.from_numpy(labels[:, 2:]).long()]
+	data = TensorDataset(*selected)
+	return data
 
 def get_tuple_frequency(dataset_lines, questions):
     # Performing Question filtering as described in Article
@@ -227,8 +300,10 @@ if __name__ == '__main__':
 	# questions = pd.read_excel(r'data/Final_Sheet_990824.xlsx', sheet_name=1, engine='openpyxl')
 	# index = get_tuple_frequency(reverb_lines, questions)
 	# index[index['Frequency']<10].to_excel('')
-	combine_with_reverb(questions_path=r'../data/normalized_questions.xlsx')
-
-	create_bertified_dataset()
+	# combine_with_reverb(questions_path=r'../data/normalized_questions.xlsx')
+	# print('hi')
+	tr = read_data('train')
+	print(len(tr))
+	# SQ_create_bertified_dataset()
 	# print(get_question_words_ids())
 	# get_relation([101, 2129, 2172, 2769, 2001, 2139, 29510, 2005, 2604, 102], [8, 9], [2054, 2029, 2073, 2043, 2339, 2040, 2129, 3183])
